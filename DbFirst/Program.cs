3710fe222f8da03b2c5fa14ccc4fa5f0c80c3aca
zuleykhapa <ya-zu@yandex.ru>
2017-02-25 13:20:18 +0300

    exercise complete

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace DbFirst
 {
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3    
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var dbContex = new PlutoDBContex();
+            var courses = dbContex.GetCourses();
+            foreach (var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+
+            var course = new Course();
+            course.Level = CourseLevel.Beginer;
+            Console.ReadLine();
         }
     }
 }
